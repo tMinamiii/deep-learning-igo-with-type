ed9b1d07279e449dc88145e832b1164697a32a17
naronA <otherthan.new@gmail.com>
2019-08-06 23:38:03 +0900

    update

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -1,6 +1,9 @@
-import copy
+from __future__ import annotations
 
-from dlgo.gotypes import Player
+import copy
+from typing import Dict, List, Optional
+
+from dlgo.gotypes import Player, Point
 
 
 class Move():
@@ -12,21 +15,21 @@ class Move():
         self.is_resign = is_resign
 
     @classmethod
-    def play(cls, point) -> 'Move':
+    def play(cls, point) -> Move:
         return Move(point=point)
 
     @classmethod
-    def pass_turn(cls) -> 'Move':
+    def pass_turn(cls) -> Move:
         return Move(is_pass=True)
 
     @classmethod
-    def resign(cls) -> 'Move':
+    def resign(cls) -> Move:
         return Move(is_resign=True)
 
 
 class GoString():
-    def __init__(self, color, stones, liberties):
-        self.color = color
+    def __init__(self, color: Player, stones, liberties):
+        self.color: Player = color
         self.stones = set(stones)
         self.liberties = set(liberties)
 
@@ -36,7 +39,7 @@ class GoString():
     def add_liberty(self, point) -> None:
         self.liberties.add(point)
 
-    def merged_with(self, go_string: 'GoString') -> 'GoString':
+    def merged_with(self, go_string: GoString) -> GoString:
         assert go_string.color == self.color
         combined_stones = self.stones | go_string.stones
         return GoString(
@@ -46,7 +49,41 @@ class GoString():
         )
 
 
+class Board():
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid: Dict[Point, GoString] = {}
 
+    def place_stone(self, player: Player, point: Point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_same_color: List[GoString] = []
+        adjacent_opposite_color: List[GoString] = []
+        liberties: List[Point] = []
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string = self._grid.get(neighbor)
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+                else:
+                    if neighbor_string not in adjacent_opposite_color:
+                        adjacent_opposite_color.append(neighbor_string)
+            new_string = GoString(player, [point], liberties)
 
+    def is_on_grid(self, point: Point):
+        return None
 
+    def get(self, point: Point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
 
+    def get_go_string(self, point: Point) -> Optional[GoString]:
+        go_str = self._grid.get(point)
+        return go_str
--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -1,18 +1,21 @@
-import enum
+from __future__ import annotations
+
 from collections import namedtuple
+from enum import IntEnum
+from typing import List
 
 
-class Player(enum.Enum):
+class Player(IntEnum):
     d_black = 1
     d_white = 2
 
     @property
-    def other(self):
+    def other(self) -> Player:
         return Player.d_black if self == Player.d_white else Player.d_white
 
 
 class Point(namedtuple('Point', 'row col')):
-    def neighbors(self):
+    def neighbors(self) -> List[Point]:
         return [
             Point(self.row - 1, self.col),
             Point(self.row + 1, self.col),
